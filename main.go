91d3d95492e84a1be8ed7259d7c91446c5ab9c94
asorokin <a40in@inbox.ru>
2020-07-13 17:32:52 +0300

    -

--- main.go
+++ main.go
@@ -26,7 +26,6 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			<title>Сиськи</title>
 		</head>
 		<body>
-		<h1>Опа! Сиськи!</h1>
 		<svg xmlns="http://www.w3.org/2000/svg" style="stroke: grey; fill: white; stroke-width: 0.7" width="%d" height="%d">`, width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
